d8c7167b9d0a59731bb3aed1eae28cb1a0ee1b09
Delion <21adtydwf@gmail.com>
2025-11-03 07:27:25 +0700

    update: fungsi square

--- Theory/sorted-squared-array.py
+++ Theory/sorted-squared-array.py
@@ -1,3 +1,15 @@
-sorted_array = list(map(int, input().split()))
-sorted_array = [x ** 2 for x in sorted_array]
-print(sorted_array)
+random_array = list(map(int, input().split()))
+# sorted_array = [x ** 2 for x in sorted_array]
+# print(sorted_array)
+
+def sortSquared(arrayInput):
+    return [x ** 2 for x in arrayInput]
+
+sortSquare = sortSquared(random_array)
+
+for i in range(len(sortSquare)):
+    for j in range(len(sortSquare) - 1):
+        if sortSquare[j] > sortSquare[j + 1]:
+            sortSquare[j + 1], sortSquare[j] = sortSquare[j], sortSquare[j + 1]
+
+print(*sortSquare)
