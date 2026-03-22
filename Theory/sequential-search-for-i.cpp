d4f076534be302cfdc453100ce8c6d7ae3608768
Delion <21adtydwf@gmail.com>
2026-03-22 19:52:23 +0700

    Add sequential search implementations using sentinel method

---
+++ Theory/sequential-search-for-i.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+int seq_search_sentinel(int array[], int n, int key)
+{
+    array[n] = key;
+
+    int i;
+
+    for (i = 0; array[i] != key; i++)
+    {
+    }
+
+    if (i < n)
+    {
+        return i;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    int array[21] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+                    200, 300, 400, 5006, 110, 550, 440, 330, 331, 41};
+
+    int n = 20;
+    int key;
+
+    cout << "Enter value to find: ";
+    cin >> key;
+
+    int result = seq_search_sentinel(array, n, key);
+
+    if (result == -1)
+        cout << "Number not found" << endl;
+    else
+        cout << "Found at index " << result << endl;
+
+    return 0;
+}
---
+++ Theory/sequential-search-for-pointer.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int seq_sentinel_search(int array[], int n, int key)
+{
+    int *p;
+
+    array[n] = key; 
+
+    for (p = array; *p != key; p++)
+        ; 
+
+    if (p - array < n)
+        return p - array; 
+    else
+        return -1; 
+}
+
+int main()
+{
+    int array[21] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+                    200, 300, 400, 5006, 110, 550, 440, 330, 331, 41};
+
+    int n = 20;
+    int key;
+
+    cout << "Enter value to find: ";
+    cin >> key;
+
+    int result = seq_sentinel_search(array, n, key);
+
+    if (result == -1)
+        cout << "Number not found" << endl;
+    else
+        cout << "Found at index " << result << endl;
+
+    return 0;
+}
