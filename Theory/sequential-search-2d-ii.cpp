a73a8247e3bb8f10d4346be1528854e5c823c6cc
Delion <21adtydwf@gmail.com>
2026-03-23 07:41:09 +0700

    Add implementation of 2D sequential search using sentinel method

---
+++ Theory/sequential-search-2d-ii.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int seq_sentinel_2d(vector<vector<int>>& A, int x) {
+    int n = A.size();
+
+    for (int i = 0; i < n; i++) {
+        A[i].push_back(x);
+
+        int j = 0;
+        while (A[i][j] != x) j++;
+
+        if (j < (int)A[i].size() - 1) {
+            A[i].pop_back();
+            return i;
+        }
+
+        A[i].pop_back();
+    }
+
+    return -1;
+}
+
+int main() {
+    int row, col, x;
+    cout << "Masukkan jumlah baris: ";
+    cin >> row;
+    cout << "Masukkan jumlah kolom: ";
+    cin >> col;
+
+    vector<vector<int>> A(row, vector<int>(col));
+    cout << "Masukkan elemen matriks:\n";
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Masukkan elemen yang dicari: ";
+    cin >> x;
+
+    int result = seq_sentinel_2d(A, x);
+    if (result != -1)
+        cout << "Elemen ditemukan pada baris ke-" << result << endl;
+    else
+        cout << "Elemen tidak ditemukan\n";
+
+    return 0;
+}
