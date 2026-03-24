5fa06b10c8b61cdbbc47ef4e61feaae710e9055b
Delion <21adtydwf@gmail.com>
2026-03-24 15:06:21 +0700

    add: hitung vokal tapi iteratif

---
+++ TLX/hitung-vokal-ii.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    int banyaknya_vokal = 0;
+
+    for (char c : s) {
+        if (c == 'a' || c == 'i' || c == 'u' || c == 'e' || c == 'o') {
+            banyaknya_vokal ++;
+        }
+    }
+
+    cout << banyaknya_vokal << endl;
+}
