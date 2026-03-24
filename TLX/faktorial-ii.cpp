e85cac43a2805a640731589a17e0d667ee60b23f
Delion <21adtydwf@gmail.com>
2026-03-24 14:48:30 +0700

    add: factorial i, factorial ii

---
+++ TLX/faktorial-ii.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long faktorial(int n) {
+    if (n == 1) return 1;
+    else return faktorial(n - 1) * n;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    cout << faktorial(n) + faktorial(m) << endl;
+}
